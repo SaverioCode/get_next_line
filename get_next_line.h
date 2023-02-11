ee5e67f77a8e94ec86ed1128b762aa50cc7bd45f
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-02-11 15:28:37 +0100

    update

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/11 15:25:00 by fgarzi-c          #+#    #+#             */
+/*   Updated: 2023/02/11 15:26:25 by fgarzi-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+char	*get_next_line(int fd);
+
+#endif
