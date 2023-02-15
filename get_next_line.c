ecdd5f2a7d43417f71c1ec5d1887b287a86ab3d5
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-02-15 01:30:16 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,19 @@
 /*   By: fgarzi-c <fgarzi-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 15:18:07 by fgarzi-c          #+#    #+#             */
-/*   Updated: 2023/02/15 00:18:26 by fgarzi-c         ###   ########.fr       */
+/*   Updated: 2023/02/15 01:30:02 by fgarzi-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int	ft_isprint(int c)
+{
+	if ((c > 31 && c < 127) || (c > 7 && c < 14))
+		return (1);
+	else
+		return (0);
+}
 
 char	*get_next_line(int fd)
 {
@@ -18,15 +26,13 @@ char	*get_next_line(int fd)
 	char		buf[BUFFER_SIZE + 1];
 	ssize_t		size;
 
-	if (read(fd, 0, 0) < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
 	if (!s_buf)
 		s_buf = "";
 	size = read(fd, buf, BUFFER_SIZE);
+	if (size < 0 || !ft_isprint(*buf))
+		return (NULL);
 	buf[BUFFER_SIZE] = 0;
 	s_buf = ft_strjoin(s_buf, buf);
-	if (size == -1 && !*s_buf)
-		return (NULL);
 	while (!ft_strchr(s_buf, 10))
 	{
 		if (!size)
@@ -38,3 +44,4 @@ char	*get_next_line(int fd)
 		return (ft_get_line(&s_buf));
 	return (NULL);
 }
+
